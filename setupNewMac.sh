313a9e13778babc5a76ac99d405c8f173f04f49f
Dennis Brändle <Dennis@Denniss-MBP.fritz.box>
2015-05-25 11:45:20 +0200

    initial commit

---
+++ setupNewMac.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#chmod a+x setupNewMac.sh
+
+./brew.sh
+
+git clone https://github.com/rupa/z.git ~/code/z
+chmod +x ~/code/z/z.sh
+
+sh .osx
+./symlinks.sh
+
+###############################################################################
+# Kill affected applications                                                  #
+###############################################################################
+
+for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" \
+	"Dock" "Finder" "Google Chrome" "Google Chrome Canary" "Mail" "Messages" \
+	"Opera" "Safari" "SizeUp" "Spectacle" "SystemUIServer" "Terminal" \
+	"Transmission" "Twitter" "iCal"; do
+	killall "${app}" > /dev/null 2>&1
+done
+echo "Done. Note that some of these changes require a logout/restart to take effect."
